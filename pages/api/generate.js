fa9675c91cf52df8f41953b682f0026ee0847c48
Jake Rush <jaker@eventbrite.com>
2022-12-08 21:29:17 -0500

    block by bannana dev

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,34 +6,7 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 const basePromptPrefix = `
-Write a letter from Jinx. At the end of the letter, include a crazy P.S. line that eludes to the upcoming chaos of the future. Make the letter include some lines of dialog from Netflix's Arcane TV Show.
-Jinx: a 26 year-old girl that lives to wreak havoc without care for the consequences is writing from Zaun.
-A Few Lines of Dialog from Jinx: 
-- "I Feel Like You & I Got Off On The Wrong Arm. Maybe We Should Try The Other."
-- "Oh, You Know. Here, There, Chasing Down Dead Ends. And Guess What? They're Not All Dead."
-- "You Never Left. I Always Heard You. Shadows In The Streets, Prickles On The Back Of My Neck. Your Voice, Pushing Me, Picking Me Up When All The Colors Were Black. You're The Reason I'm Still Alive."
-- "She Left Me. She Is Not My Sister Anymore."
-- "I Thought, Maybe You Could Love Me Like You Used To, Even Though I'm Different. But You Changed Too, So, Here's To The New Us.
-- "It Wasn't Her. It Wasn't. I Know, Just Some Wannabe Street Trash. I Got Confused, That's All. Now, He Thinks I'm Weak. 'Sevika Will Clean It Up.' Sevika's A Regular Johnny-On-The-Spot. I'm Not Weak. And I'm Gonna Show Him. Oh, I'm Gonna Show Him. You'll See."
-- "You're A Class Act Sister. Sister, Thought I Missed Her. Bet You Wouldn't Miss Her."
-- "I Made Her A Snack. Sheesh, I'm Not That Crazy."
-- " Really thought I buried this place. But I should have known better."
-- "Nothing ever stays dead."
-- "For now. Maybe forever."
-- "Wanna know a secret?
-- "Silco thinks he made Jinx, with all his rants and his hard-won lessons. "Excise your doubts, Jinx.", "Be what they fear, Jinx." Like everything was the same as when Vander left him. But he didn't make Jinx. You did"
-- "I Made Her A Snack. Sheesh, I'm Not That Crazy."
-- "You wouldn't lie to me. Not again."
-
-Me: Hi Jinx, How's Life? 
-Jinx: Hiiiii :)
-
-Oh, You Know. Here, There, Chasing Down Dead Ends. And Guess What? They're Not All Dead. 
-
-Here's to the new us!
-
-Me: What are you doing next year?
-Jinx: I'm going to try and take over the world! P.S. I'm going to start by taking over Zaun ;)
+Write a 1,000 word newsletter email base on the following audio transcript.
 
 Me:`;
 const generateAction = async (req, res) => {
@@ -57,7 +30,7 @@ const generateAction = async (req, res) => {
     `;
 
   const secondPromptCompletion = await openai.createCompletion({
-    model: "text-davinci-002",
+    model: "text-davinci-003",
     prompt: `${secondPrompt}`,
     temperature: 0.85,
     max_tokens: 550,
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,20 @@ import Image from "next/image";
 import buildspaceLogo from "../assets/buildspace-logo.png";
 import { useState } from "react";
 
+const toBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
 const Home = () => {
   const [userInput, setUserInput] = useState("");
   const [apiOutput, setApiOutput] = useState("");
   const [isGenerating, setIsGenerating] = useState(false);
+  const [whisperApiOutput, setWhisperApiOutput] = useState("");
+  const [isTranscribing, setIsTranscribing] = useState(false);
 
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
@@ -28,6 +38,33 @@ const Home = () => {
     setIsGenerating(false);
   };
 
+  const callWhisperEndpoint = async () => {
+    isTranscribing(true);
+    const fileInput = document.getElementById("file-upload");
+    const audioFile = fileInput.files[0];
+    console.log("audio", audioFile);
+    const base64data = await toBase64(audioFile);
+    console.log("base64data", base64data);
+    const base64String = base64data.split(",")[1];
+    console.log("base64String", base64String);
+
+    console.log("Calling Replicate...");
+    const response = await fetch("/api/transcribe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ audio: base64String }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output);
+
+    setWhisperApiOutput(`${output}`);
+    setIsTranscribing(false);
+  };
+
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
@@ -40,12 +77,48 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Letters from Zaun</h1>
+            <h1>Audio to Email</h1>
           </div>
           <div className="header-subtitle">
-            <h2>What will happen in the Aftermath of Arcane?</h2>
+            <h2>Generate newsletters from your podcasts</h2>
           </div>
         </div>
+        <div className="upload-container">
+          <div className="generate">
+            <p>Upload an audio file</p>
+          </div>
+          <input
+            id="file-upload"
+            name="file-upload"
+            type="file"
+            accept="audio/*"
+            className="sr-only"
+          />
+        </div>
+        <div className="prompt-buttons">
+          <a
+            className={
+              isTranscribing ? "generate-button loading" : "generate-button"
+            }
+            onClick={callWhisperEndpoint}
+          >
+            <div className="generate">
+              {isGenerating ? <span class="loader"></span> : <p>Transcribe</p>}
+            </div>
+          </a>
+        </div>
+        {whisperApiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{whisperApiOutput}</p>
+            </div>
+          </div>
+        )}
         <div className="prompt-container">
           <textarea
             placeholder="start typing here"
